796387ed16441b87dbba9c62a7df1b214fb7dcb0
richard-nguyen00 <106111300+richard-nguyen00@users.noreply.github.com>
2024-04-02 01:28:05 -0500

    Added Main Screen (WIP)

--- frontend/src/Components/Main/Main.jsx
+++ frontend/src/Components/Main/Main.jsx
@@ -3,7 +3,7 @@ import './Main.css'; // Import Main.css for styling
 
 const Main = () => {
   return (
-    <div className="container">
+    <div className="containerMain">
       <h1>Hello, World!</h1>
     </div>
   );
